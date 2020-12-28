caa7ead5d9cf1422d3dd76e166d4a2f9a935f084
Aleksandar Hough <45799937+Eous638@users.noreply.github.com>
2020-12-28 21:33:28 +0100

    map

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import MapView from 'react-native-maps';
 
 const home = () => {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+              <MapView
+                initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+                    }}
+                />
         </View>
     )
 }
